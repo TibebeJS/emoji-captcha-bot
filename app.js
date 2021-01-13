af1de0e625ffdbdf43e187cba582d2540933bf90
TibebeJs <Tibebes.js@gmail.com>
2021-01-13 22:28:12 +0300

    inline button handling logic

--- app.js
+++ app.js
@@ -2,13 +2,42 @@ const { Telegraf } = require('telegraf')
 const { config } = require('dotenv')
 config()
 
-const { pickRandomEmojis, generateBtns } = require('./utils')
+const { generateBtns } = require('./utils')
+const { EmojiCaptcha } = require('./core')
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
-bot.on('message', async (ctx) => {
-    const emojis = await pickRandomEmojis()
-    await bot.telegram.sendMessage(700867545, 'solve this:', generateBtns(emojis))
+const captcha = new EmojiCaptcha()
+
+bot.action(/([a-fA-F0-9]{8,16})\.png/, async ctx => {
+    try {
+        const isCorrect = captcha.check(ctx.match[0])
+        if (isCorrect) {
+            await ctx.answerCbQuery(`✅ That's correct!`)
+        }
+        else {
+            await ctx.editMessageText(
+                `
+Please select the emojis you see here:
+
+${captcha.presentedEmojis.map(x => x.hex).join('-')}
+
+<b>Attempts left:</b> ${captcha.attemptsLeft}
+`,
+                {
+                    reply_markup: ctx.callbackQuery.message.reply_markup,
+                    parse_mode: "HTML"
+                }
+            )
+            await ctx.answerCbQuery(`❌ That's incorrect! focus!`)
+        }
+    } catch (error) {
+        if (error.message == 'no attempts left') {
+            await ctx.editMessageText(`You have failed the captcha.`)
+        } else {
+            console.error(error)
+        }
+    }
 })
 
 bot.launch()
