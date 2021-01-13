1e6ffda06e6e26123a88cc100a11f9135e0debf1
TibebeJs <Tibebes.js@gmail.com>
2021-01-13 22:28:43 +0300

    refactor utils module

--- utils/buttons.js
+++ utils/buttons.js
@@ -2,7 +2,9 @@ const { Markup } = require('telegraf')
 const chunk = require('lodash.chunk');
 
 module.exports = (emojis) => {
-    return Markup.inlineKeyboard(
-        chunk(emojis.map(emoji => Markup.button.callback(emoji.hex, emoji.char)), 4)
-    )
+    return {
+        ...Markup.inlineKeyboard(
+            chunk(emojis.map(emoji => Markup.button.callback(emoji.hex, emoji.char)), 3)
+        ), parse_mode: "HTML",
+    }
 }
--- utils/pick.js
+++ utils/pick.js
@@ -1,32 +1,16 @@
-const fs = require('fs').promises
+const fs = require('fs')
 const chunk = require('lodash.chunk');
-function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+const shuffle = require('./shuffle')
 
-    while (0 !== currentIndex) {
-
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
-
-    return array;
-}
-
-module.exports = async function () {
-    const emojis = shuffle(await fs.readdir('./emojis')).slice(0, 9)
+module.exports = function () {
+    const emojis = shuffle(fs.readdirSync('./emojis')).slice(0, 9)
 
     return emojis.map(emoji => ({
         char: emoji,
-        hex: chunk(
-            emoji
-                .slice(0, emoji.length - 4), 8)
-                .map(
-                    part => String.fromCodePoint(parseInt(part.join(''), 16))
-                )
-                .join('')
+        hex: chunk(emoji.slice(0, emoji.length - 4), 8)
+            .map(
+                part => String.fromCodePoint(parseInt(part.join(''), 16))
+            )
+            .join('')
     }))
 }
