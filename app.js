479302c49564ff9d5789283b61550664c24e7f1f
TibebeJs <Tibebes.js@gmail.com>
2021-12-27 15:09:10 +0300

    💪 WIP(core): restrict new members when joining a group

--- app.js
+++ app.js
@@ -22,23 +22,37 @@ bot.use((new LocalSession({
   }
 })).middleware())
 
-bot.use(async (ctx, next) => {
-  ctx.state.captcha = Object.keys(ctx.session).length === 0 ? new EmojiCaptcha() : EmojiCaptcha.from(ctx.session)
-  const result = await next()
-  ctx.session = JSON.parse(JSON.stringify(ctx.state.captcha))
-  return result
-})
+// bot.use(async (ctx, next) => {
+//   ctx.state.captcha = Object.keys(ctx.session).length === 0 ? new EmojiCaptcha() : EmojiCaptcha.from(ctx.session)
+//   const result = await next()
+//   ctx.session = JSON.parse(JSON.stringify(ctx.state.captcha))
+//   return result
+// })
 
 bot.on('message', async (ctx) => {
-  if (ctx.state.captcha.status === CaptchaStatus.PASS) {
-    await ctx.reply('You have already passed the captcha.')
-  } else if (ctx.state.captcha.status === CaptchaStatus.FAILED) {
-    await ctx.reply('You have failed the captcha.')
-  } else if (ctx.state.captcha.status === CaptchaStatus.EXPIRED) {
-    await ctx.reply('The captcha has expired.')
-  } else {
-    await ctx.reply(
-      `
+  if (ctx.message && ctx.message.new_chat_members && ctx.message.new_chat_members.length) {
+    for (const newMember of ctx.message.new_chat_members) {
+      try {
+        if (newMember.is_bot) {
+          await ctx.replyWithHTML(`@${newMember.username} is a bot.`)
+        } else {
+          await ctx.telegram.restrictChatMember(ctx.message.chat.id, newMember.id)
+          await ctx.replyWithHTML(`Dear &lt;${newMember.first_name}&gt;, new members are automatically muted.
+Please solve the following captcha to be unmuted.
+      `)
+
+          ctx.state.captcha = Object.keys(ctx.session).length === 0 ? new EmojiCaptcha() : EmojiCaptcha.from(ctx.session)
+          ctx.session = JSON.parse(JSON.stringify(ctx.state.captcha))
+
+          if (ctx.state.captcha.status === CaptchaStatus.PASS) {
+            await ctx.reply('You have already passed the captcha.')
+          } else if (ctx.state.captcha.status === CaptchaStatus.FAILED) {
+            await ctx.reply('You have failed the captcha.')
+          } else if (ctx.state.captcha.status === CaptchaStatus.EXPIRED) {
+            await ctx.reply('The captcha has expired.')
+          } else {
+            await ctx.reply(
+              `
 Please select the emojis you see here:
 
 ${ctx.state.captcha.presentedEmojis.map(x => x.hex).join('-')}
@@ -46,10 +60,39 @@ ${ctx.state.captcha.presentedEmojis.map(x => x.hex).join('-')}
 <b>Attempts left:</b> ${ctx.state.captcha.attemptsLeft}
 `,
 
-      generateBtns(ctx.state.captcha.choices)
-    )
+              generateBtns(ctx.state.captcha.choices)
+            )
+          }
+
+        }
+      } catch (err) {
+        console.error(err)
+      }
+    }
   }
 })
+
+// bot.on('message', async (ctx) => {
+//   if (ctx.state.captcha.status === CaptchaStatus.PASS) {
+//     await ctx.reply('You have already passed the captcha.')
+//   } else if (ctx.state.captcha.status === CaptchaStatus.FAILED) {
+//     await ctx.reply('You have failed the captcha.')
+//   } else if (ctx.state.captcha.status === CaptchaStatus.EXPIRED) {
+//     await ctx.reply('The captcha has expired.')
+//   } else {
+//     await ctx.reply(
+//       `
+// Please select the emojis you see here:
+
+// ${ctx.state.captcha.presentedEmojis.map(x => x.hex).join('-')}
+
+// <b>Attempts left:</b> ${ctx.state.captcha.attemptsLeft}
+// `,
+
+//       generateBtns(ctx.state.captcha.choices)
+//     )
+//   }
+// })
 
 bot.action(/([a-fA-F0-9]{8,16})\.png/, async ctx => {
   try {
@@ -80,5 +123,17 @@ ${ctx.state.captcha.presentedEmojis.map(x => x.hex).join('-')}
     }
   }
 })
+
+// await ctx.reply(
+//   `
+// Please select the emojis you see here:
+
+// ${ctx.state.captcha.presentedEmojis.map(x => x.hex).join('-')}
+
+// <b>Attempts left:</b> ${ctx.state.captcha.attemptsLeft}
+// `,
+
+//   generateBtns(ctx.state.captcha.choices)
+// )
 
 bot.launch()
